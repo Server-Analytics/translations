e6b5a2ccc0ba3516e835a73e1ff884e0ade25122
UBrainDev <ubrainfr@gmail.com>
2021-04-21 13:57:35 +0200

    New transcripts

--- en.js
+++ en.js
@@ -38,12 +38,12 @@ module.exports = {
     help: {
         help_embed_title: "Server Analytics help page",
         help_embed_field1_name: "Server statistics",
-        help_embed_field1_content: "`${prfx}stats` - Server detailed statistics\n> `${prfx}top` - Server members leaderboards\n> `${prfx}graph` - Generate a graphic\n> `${prfx}forecast` - Forecasts\n> `${prfx}trends` - Server activity trends",
+        help_embed_field1_content: "`${prfx}stats` - Server detailed statistics\n> `${prfx}top` - Server members leaderboards\n> `${prfx}graph` - Generate charts\n> `${prfx}forecast` - Forecasts\n> `${prfx}trends` - Server activity trends",
         help_embed_field2_name: "Personal statistics",
         help_embed_field2_content: "`${prfx}me` - Display your statistics on this server\n> `${prfx}u [@member]` - Display the statistics of a member",
         help_embed_field3_name: "Other commands",
         help_embed_field3_content: "`${prfx}counter` - Create and manage counter channels\n> `${prfx}invite` - Invite the bot on your server\n> `${prfx}set` - Modify Server Analytics configuration",
-        help_embed_field4_name: "Liens importants",
+        help_embed_field4_name: "Important links",
         help_embed_field4_content: "[`Website`](https://discordanalytics.fr) • [`Our Server`](https://discord.gg/g62eVMr) • [`Invite the bot`](https://cutt.ly/regmeY2)",
         help_embed_field4_premium_since: "ServerPremium for",
         help_embed_field4_get_premium: "**» Not yet premium?** *Type s.premium*",
@@ -84,11 +84,136 @@ module.exports = {
     },
 
     set: {
-        set_lang_list_title: "Modifier la langue du bot",
-        set_lang_list_description: "Modifiez la langue du bot en tapant `${prfx}set lang [langue]`, par exemple `${prfx}set lang en` pour définir la langue du serveur en Anglais.",
-        set_lang_error_wrong_countrycode: "La langue entrée n'existe pas ou n'est pas encore disponnible ! Entrées possibles:",
-        set_lang_update_embed_title: "Langue du bot modifiée !",
-        set_lang_update_embed_content: "Les commandes seront désormais affichées en"
-    }
+        set_updated_config: "Configuration modifiée",
+        set_lang_list_title: "Change the bot language",
+        set_lang_list_description: "Change the bot language by typing `${prfx}set lang [language]`, for example `${prfx}set lang en` to set the server language to English.",
+        set_lang_error_wrong_countrycode: "The input language doesn't exist or isn't yet available ! Possible inputs:",
+        set_lang_update_embed_title: "Bot language edited !",
+        set_lang_update_embed_content: "Commands will now be displayed in",
+        set_compare_disabled: "**D'accord!** Il est désormais **impossible** de comparer vos statistiques sur d'autres serveurs. *Retapez la commande pour réactiver la fonctionnalité.*",
+        set_compare_enabled: "**D'accord!** Il est à nouveau **possible** de comparer les statistiques de votre serveur. *Retapez la commande pour désactiver la fonctionnalité.*",
+        set_help_embed_title: "Configuration de ${botName}",
+        set_help_embed_allowCompare: "Autoriser/Refuser d'autres serveurs à comparer vos statistiques",
+        set_help_embed_hoverColor: "Changer la couleur du fond au survol du serveur sur notre site web",
+        set_help_embed_lang: "Modifier la langue de Server Analytics. (Switch to english)"
+    },
 
+    stats: {
+        stats_error_compare_not_exist: "L'ID entré n'est pas celui d'un serveur valide où Server Analytics n'est pas dessus...\n> **Comment trouver l'ID ?** https://i.imgur.com/IsSzZ2O.png",
+        stats_error_compare_disabled: "Vous essayez de comparer vos statistiques avec un serveur qui n'autorise pas cette fonctionnalité...\n> 💡 **»** *Vous pouvez vous aussi refuser d'être comparé en tapant `${prfx}set allowCompare`.*",
+        stats_error_compare_not_member: "Pour des raisons de confidentialité, tu dois être membre du serveur que tu souhaites comparer pour procéder à une comparaison.",
+        stats_error_restricted_premium: "Ce tableau statistique est réservé aux serveurs détenant le Server Premium. Pour plus d'infos, tapez ` s.premium `",
+        stats_error_restricted_dev: "L'accès à ce tableau statistique a été restreint. Celui-ci est peut-être en maintenance ou en cours de création.. Revenez plus tard !",
+        stats_error_no_datas: "Je manque de statistiques ! Laisse-moi juste quelques minutes pour que je commence à relever les statistiques du serveur...",
+        stats_error_empty_board: "Le format du tableau statistique est incorrect: Aucune statistique à afficher...",
+        stats_error_unknown_method: "La méthode statitique personnalisée entrée sur l'objet n'existe pas, vérifiez la structure du tableau.",
+        stats_embed_title: "Tableau Statistique",
+        stats_embed_compared_with: "Statistiques comparées avec",
+        stats_embed_compare_instr: "Comparez avec un autre serveur",
+        stats_embed_content_chart_timerange: "Ces 6 derniers jours",
+        stats_embed_content_empty_logs: "Vous n'avez rejoint aucun vocal ces 31 derniers jours.",
+        stats_embed_chart_default_name: "Graphique sur les Messages Envoyés",
+        stats_embed_footer: "Généré en",
+        stats_help_embed_title: "Panneaux Statistiques de ${guildName}",
+        stats_help_embed_description: "**Consultez vos statistiques** et celles du serveur à l'aide des panneaux standards et personnalisés.",
+        stats_help_embed_field_your_boards: "Vos panneaux statistiques",
+        stats_help_embed_field_your_boards_empty: "**Whoops !** Il n'est pas encore possible de créer ses propres tableaux statistiques.. Reviens plus tard !",
+        stats_help_embed_footer: "Commande expérimentale",
+
+        // Standard stat board "messages" 
+        stats_board_messages_category1: "Messages Envoyés",
+        stats_board_messages_category2: "Mentions et Activité",
+        stats_board_messages_category3: "Supprimés et moyennes",
+        stats_board_messages_category1_subcategory1: "✉ • Messages Envoyés:",
+        stats_board_messages_category1_displayedText: "\n> **Vous »**", // You sent » (x messages)
+        stats_board_messages_category1_subcategory2: "📊 • Graphique:",
+        stats_board_messages_category2_subcategory1: "📌 • Mentions:",
+        stats_board_messages_category2_subcategory2: "👥 • Membres Actifs:",
+        stats_board_messages_category3_subcategory1: "🗑 • Supprimés:",
+        stats_board_messages_category3_subcategory1_unitName: "supprimés",
+        stats_board_messages_category3_subcategory2: "📋 • En moyenne:",
+        stats_board_messages_category3_subcategory2_displayedText: "> `{%a%}` messages/**mois**\n> `{%b%}` messages/**jour**\n> `{%d%}` messages/**heure**\n> `{%c%}` messages/**membre**",
+
+        // Standard stat board "members"
+        stats_board_members_category1: "Nombre de Membres",
+        stats_board_members_category2: "Départs et arrivées",
+        stats_board_members_category3: "Graphiques",
+        stats_board_members_category1_subcategory1: "👥 • Evolution:",
+        stats_board_members_category1_subcategory1_displayedText: "> Membres",
+        stats_board_members_category1_subcategory1_unitName: "membres",
+        stats_board_members_category1_subcategory2: "📋 • En moyenne:",
+        stats_board_members_category1_subcategory2_displayedText: "*Nouveaux membres:*\n> `+ {%a%}` membres/**mois**\n> `+ {%b%}` membres/**jour**\n> `+ {%d%}` membres/**heure**",
+        stats_board_members_category2_subcategory1: "<:le:807632343497703454> • Arrivés:",
+        stats_board_members_category2_subcategory1_unitName: "arrivées",
+        stats_board_members_category2_subcategory2: "<:j_:807632343207772190> • Départs:",
+        stats_board_members_category2_subcategory2_unitName: "départs",
+        stats_board_members_category3_subcategory1: "📈 • Arrivées:",
+        stats_board_members_category3_subcategory2: "📉 • Départs:",
+
+        // Standard stat board "voice"
+        stats_board_voice_category1: "Statistiques Vocales",
+        stats_board_voice_category2: "Vos Statistiques Vocales",
+        stats_board_voice_category3: "Sessions Vocales",
+        stats_board_voice_category1_subcategory1: "🔊 • Temps en vocal:",
+        stats_board_voice_category1_subcategory1_displayText: "\n> **Vous »** ",
+        stats_board_voice_category1_subcategory2: "📊 • Graphique:",
+        stats_board_voice_category2_subcategory1: "👥 • Temps passé en vocal:",
+        stats_board_voice_category2_subcategory2: "📊 • Graphique:",
+        stats_board_voice_category3_subcategory1: "📚 • Vos derniers vocaux:",
+
+    },
+
+    invite: {
+        invite_message_title: "Ajoutez ${botName}",
+        invite_message_add_bot: "Ajouter le bot",
+        invite_message_dashboard: "Dashboard",
+        invite_message_discord_server: "Serveur Support"
+    },
+
+    graph: {
+        graph_error_no_datas: "Je n'ai pas assez de statistiques pour créer un graphique... Reviens plus tard :(",
+        graph_error_low_timerange: "Il n'est pas encore possible d'afficher des graphiques sur une période inférieure à **6 jours**",
+        graph_error_high_timerange: "Il n'est pas encore possible d'afficher des graphiques sur une période suppérieure à **9999 jours**",
+        graph_error_premium_timerange: "Seuls les serveurs avec un abonnement premium peuvent afficher des graphiques sur plus de **31 jours**!",
+        graph_help_embed_title: "Générer un graphique",
+        graph_help_embed_content: "Affichez un graphique sur **${x}** jours:",
+        graph_help_embed_available_graphs: "Graphiques disponibles",
+
+        // Do not edit "type", "columnName", "format" and "normalizeFactor" proprieties!
+        graph_list: {
+            messages: {
+                type: "guild",
+                graphName: "Graphique sur les Messages Envoyés",
+                graphDescription: "Graphique sur les messages envoyés sur ce serveur.",
+                columnName: "messages"
+            },
+            members: {
+                type: "guild",
+                graphName: "Graphique sur l'évolution du nombre de Membres",
+                graphDescription: "Graphique sur l'évolution du nombre de membres dans ce serveur.",
+                columnName: "membres",
+                processMethod: "set"
+            },
+            mentions: {
+                type: "guild",
+                graphName: "Graphique sur le Nombre de Mentions",
+                graphDescription: "Graphique sur le nombre de mentions envoyées dans ce serveur.",
+                columnName: "mentions"
+            },
+            myMessages: {
+                type: "user",
+                graphName: "Graphique du Nombre de Messages",
+                graphDescription: "Graphique sur le nombre de messages que vous avez envoyé sur ce serveur.",
+                columnName: "messages"
+            },
+            voiceTime: {
+                type: "guild",
+                graphName: "Graphique sur le temps passé en vocal",
+                graphDescription: "Graphique sur le temps passé en vocal sur ce serveur.",
+                columnName: "voice_time",
+                format: "time",
+                normalizeFactor: 1000
+            }
+        }
+    },
 }
