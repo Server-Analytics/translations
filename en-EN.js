8da3281ec0a4f829323b2ba30a3702cfca06862e
shiirudo@outlook.fr <shiirudo@outlook.fr>
2022-05-22 16:53:42 +0200

    Update en-EN.js
    (Je suis pas sûr de la formulation de certains, mais j'ai fait de mon mieux)

--- en-EN.js
+++ en-EN.js
@@ -142,31 +142,31 @@ module.exports = {
             fetch_error: "**Whoops!** The required data could not be retrieved to create a graph.. *If the problem happens again, please report the bug on the support server.*"
         },
         help_screen: {
-            embed_title: "Créer un graphique",
-            embed_description: "**Représentez** vos statistiques ou celles du serveur sous forme de graphiques. Commencez par choisir une **préconfiguration** :",
-            image_title: "Graphiques",
-            embed_field_server_graph: "Graphiques du serveur",
-            embed_field_user_graph: "Graphique de vos stats",
-            components_guild_graph_select_menu: "Choisir un graphique sur les stats du serveur..",
-            components_user_graph_select_menu: "..ou un graphique avec vos statistiques !"
+            embed_title: "Create a graph",
+            embed_description: "**Display** your stats or the server's stats with graphs. Start by selecting a **configuration** :",
+            image_title: "Graphs",
+            embed_field_server_graph: "Server graphs",
+            embed_field_user_graph: "Your stats' graphs",
+            components_guild_graph_select_menu: "Select a server stats graph..",
+            components_user_graph_select_menu: "..or a graph with your stats!"
         },
         graph_main_screen: {
             advanced: {
-                confirm_button: "Générer",
-                advanced_button: "Options avancées",
-                advanced_embed_description: "**Adaptez** et **personnalisez** l'interprétation et l'affichage de vos graphiques.",
-                advanced_embed_options_field_title: "Options disponibles :",
+                confirm_button: "Generate",
+                advanced_button: "Advanced settings",
+                advanced_embed_description: "**Adapt** and **customize** your graphs' interpretation and display layout.",
+                advanced_embed_options_field_title: "Available options:",
                 advanced_options: {
-                    display_bar_chart: "Affichage en barres",
-                    increment_stats: "Somme cumulative",
-                    remove_inexistant_reports: "Retrait des rapports inexistants",
-                    display_ticks: "Affichage des points",
-                    group_values: "Groupement statistique"
+                    display_bar_chart: "Bar display",
+                    increment_stats: "Cumulative sum",
+                    remove_inexistant_reports: "Removing of inexistants reports",
+                    display_ticks: "Point display",
+                    group_values: "Statistical grouping"
                 },
                 timerange_select_menu: {
-                    placeholder: "Choisir un intervalle de temps..",
-                    option_title: "Affichage sur {timeRange}",
-                    premium_only_description: "Premium uniquement"
+                    placeholder: "Select a time range..",
+                    option_title: "{timeRange} display",
+                    premium_only_description: "Premium only"
                 }
             },
             export: {
@@ -191,74 +191,74 @@ module.exports = {
         },
         graph_types: {
             server_messages: {
-                title: "Messages Envoyés",
-                description: "Graphique résumant l'activité par messages du serveur.",
-                graph_name: "Résumé de l'activité par message"
+                title: "Sent Messages",
+                description: "Graph by server activity in messages.",
+                graph_name: "Summary of activity by messages"
             },
             server_voicetime: {
-                title: "Temps passé en vocal",
-                description: "Graphique résumant l'activité vocale du serveur.",
-                graph_name: "Résumé de l'activité vocale"
+                title: "Time spent in voice",
+                description: "Graph by server voice activity.",
+                graph_name: "Summary of voice activity"
             },
             server_members: {
-                title: "Membres",
-                description: "Graphique résumant le nombre de membres du serveur.",
-                graph_name: "Résumé de l'évolution du nombre de membres"
+                title: "Members",
+                description: "Graph by number of server members.",
+                graph_name: "Summary of server members evolution"
             },
             server_deleted: {
-                title: "Messages Supprimés",
-                description: "Graphique résumant le montant de messages supprimés.",
-                graph_name: "Résumé du nombre de messages supprimés"
+                title: "Deleted Messages",
+                description: "Graph by amount of deleted messages.",
+                graph_name: "Summary of deleted messages"
             },
             server_reactions: {
-                title: "Réactions",
-                description: "Graphique résumant le nombre de réactions aux messages.",
-                graph_name: "Résumé du nombre de réactions"
+                title: "Reactions",
+                description: "Graph by reactions to messages.",
+                graph_name: "Summary of reactions"
             },
             server_joined: {
-                title: "Nouveaux membres",
-                description: "Graphique résumant le nombre de nouveaux arrivants.",
-                graph_name: "Résumé du nombre de nouveaux arrivants"
+                title: "New members",
+                description: "Graph by newcomers.",
+                graph_name: "Summary of newcomers"
             },
             server_left: {
-                title: "Départs",
-                description: "Graphique résumant le nombre de membres ayant quitté.",
-                graph_name: "Résumé du nombre de départs"
+                title: "Members who left",
+                description: "Graph by members who left.",
+                graph_name: "Summary of members who left"
             },
             user_myMessages: {
-                title: "Messages Envoyés",
-                description: "Graphique résumant votre activité par messages.",
-                graph_name: "Résumé de l'activité par message"
+                title: "Sent Messages",
+                description: "Graph by your activity in messages.",
+                graph_name: "Summary of activity by messages"
             },
             user_myVoicetime: {
-                title: "Temps en vocal",
-                description: "Graphique résumant votre activité vocale.",
-                graph_name: "Résumé de l'activité vocale"
+                title: "Time spent in voice",
+                description: "Graph by your voice activity.",
+                graph_name: "Summary of voice activity"
             },
             user_myReactedCount: {
-                title: "Réactions données",
-                description: "Graphique résumant le nombre de réactions données.",
-                graph_name: "Résumé du nombre de réactions données"
+                title: "Reactions",
+                description: "Graph by reactions to messages.",
+                graph_name: "Summary of given reactions"
             },
             user_myDeletedMessages: {
-                title: "Messages Supprimés",
-                description: "Graphique résumant votre montant de messages supprimés.",
-                graph_name: "Résumé du nombre de messages supprimés"
+                title: "Deleted Messages",
+                description: "Graph by your amount of deleted messages.",
+                graph_name: "Summary of deleted messages"
             }
         }
     },
     top: {
         not_enough_data: "**Whoops!** I don't have enough data to make rankings for now.. *If the problem happens again, please report the bug on the support server.*",
         not_allowed_timerange: "**Whoops!** You can't choose a timerange longer than **{max}** days.",
-        not_allowed_timestamp_low: "**Whoops!** Vous ne pouvez pas choisir un intervalle temporel d'une durée inférieure à **{max}** jour(s).",
+        not_allowed_timestamp_low: "**Whoops!** You can't select a time range inferior to **{max}** day(s).",
         top_canvas_leaderboard_date: "Global rankings on {date}",
         top_canvas_leaderboard_temp_date: "{type} rankings from {date1} to {date2}",
         top_canvas_empty_username: "Nobody",
         top_canvas_not_found_username: "User not found",
         top_canvas_leaderboard_temp_types: {
-            "0": "complets",
-            "5": "partiels",
-            "10": "schématiques"
+            "0": "completes",
+            "5": "incompletes",
+            "10": "schematics"
         },
         pos_abbreviations: {
             "1": "st",
@@ -283,7 +283,7 @@ module.exports = {
             embed_data_FULL: "All data",
             embed_data_PARTIAL_5: "Partial data",
             embed_data_PARTIAL_10: "Schematic data",
-            embed_data_partial_disclamer: "Pour limiter la saturation du bot, certains classements sont **partiels** ou **schématiques**, les stats y sont inexactes mais suffisantes pour déterminer les rangs.",
+            embed_data_partial_disclamer: "To limit bot overload, some of the rankings are **incomplete** or **schematics**, their stats are inaccurate but sufficient enough to calculate rankings.",
             embed_data_learn_more: "Find out more",
             components_days_acronym: "d"
         }
